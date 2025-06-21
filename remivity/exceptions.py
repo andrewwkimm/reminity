1002e82084e49d90ff451c6021e54e595251a574
andrewwkimm <andrewkimka@gmail.com>
2025-06-21 04:09:50 +0000

    Add processing error

--- remivity/exceptions.py
+++ remivity/exceptions.py
@@ -5,5 +5,9 @@ class AudioExtractionError(Exception):
     """Audio extraction error."""
 
 
+class ProcessingError(Exception):
+    """Pipeline processing error."""
+
+
 class SummarizationError(Exception):
     """Text summarization error."""
