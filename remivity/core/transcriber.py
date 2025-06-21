97d00fca3dddfd39f52c52b474315550cb6faf68
andrewwkimm <andrewkimka@gmail.com>
2025-06-21 03:01:28 +0000

    Start text transcription from audio files

---
+++ remivity/core/transcriber.py
@@ -0,0 +1,22 @@
+"""The audio file transcriber."""
+
+from pathlib import Path
+
+from faster_whisper import WhisperModel
+
+
+def transcribe_audio(
+    audio_file: str | Path,
+    compute_type: str,
+    device: str,
+    model: str,
+) -> str:
+    """Transcribes an audio file to text."""
+    loaded_model = WhisperModel(
+        model_size_or_path=model, device=device, compute_type=compute_type
+    )
+
+    segments, _ = loaded_model.transcribe(str(audio_file))
+
+    transcript = " ".join(segment.text.strip() for segment in segments)
+    return transcript
